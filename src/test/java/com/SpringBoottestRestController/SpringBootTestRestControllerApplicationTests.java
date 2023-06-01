cceac12624846db7c3423f751bd2622001682384
Manjunath S.P <49641628+Usha55@users.noreply.github.com>
2023-06-02 00:17:57 +0530

    first commit

---
+++ src/test/java/com/SpringBoottestRestController/SpringBootTestRestControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBoottestRestController;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestRestControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
